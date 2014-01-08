5e0c629f61aa88be165ff6bc9cb80d622a76c16f
kdeloach <kdeloach@gmail.com>
2014-01-08 03:28:13 -0500

    Removed BasicMine

--- src/Mine.java
+++ src/Mine.java
@@ -3,50 +3,83 @@ import java.awt.Graphics;
 import java.awt.Dimension;
 import java.awt.Polygon;
 import java.awt.Point;
+import java.awt.Color;
 
-public abstract class Mine extends Bullet
+public class Mine extends Bullet
 {
-	
 	// amount of steps to take before resting
 	private int steps;
-	
-	public Mine(int x, int y, double t, double dmg, Weapon w){
+
+	public Mine(int x, int y, double t, double dmg, Weapon w)
+    {
 		super(x, y, t, dmg, w);
-		
-		steps=getDefaultSteps();
+		steps = getDefaultSteps();
 	}
-	
-	public void move(){
+
+	public void move()
+    {
 		// we want to stop moving at some point
-		if(steps<=0){
-			
+		if (steps <= 0) {
 			try {
-				for(Projectile p : weapon.ship.panel().bulletList)
-					if(p instanceof Mine && p.getId()!=getId() && p.intersects(this))
-					{
+				for (Projectile p : weapon.ship.panel().bulletList) {
+					if (p instanceof Mine && p.getId() != getId() && p.intersects(this)) {
 						p.explode();
-						explode();	
+						explode();
 					}
+                }
 			} catch (java.util.ConcurrentModificationException ie) {}
-				
 			return;
 		}
 
 		setLocation(
-			dx + steps/Const.BULLET_SPEED*Math.cos(theta),
-			dy + steps/Const.BULLET_SPEED*Math.sin(theta)
+			dx + steps / Const.BULLET_SPEED * Math.cos(theta),
+			dy + steps / Const.BULLET_SPEED * Math.sin(theta)
 		);
-		
+
 		steps--;
 	}
-		
-	///////
-	// Abstracts
-	
-	abstract public int getDefaultSpeed();
-	abstract public int getDefaultSteps();
-	//abstract public int getDefaultRange();
-	abstract public void draw(Graphics g);
-	abstract public void die();
 
+	public void draw(Graphics g)
+    {
+		g.setColor(Color.GREEN);
+
+		int off = width / 6;
+		//g.drawOval(getX()-width/2,getY()-height/2,width,height);
+
+		g.drawRect(getX() - width / 2 + off,
+		           getY() - height / 2 + off,
+		           width - off * 2,
+		           height - off * 2);
+
+		g.drawPolygon(
+			new int[]{getX() - width / 2, getX(), getX() + width / 2, getX()},
+			new int[]{getY(), getY() - height / 2, getY(), getY() + height / 2},
+			4
+		);
+	}
+
+	public void die()
+    {
+		new BasicExplosion(getX(), getY(), panel());
+	}
+
+	public int getDefaultSpeed()
+    {
+		return 15;
+	}
+
+	public int getDefaultSteps()
+    {
+		return 40;
+	}
+
+	public Dimension getSize()
+    {
+		return new Dimension(25,25);
+	}
+
+	public int getDefaultTeam()
+    {
+		return Const.PLAYER;
+	}
 }
--- src/MineLauncher.java
+++ src/MineLauncher.java
@@ -1,31 +1,31 @@
 import java.awt.*;
 
 public class MineLauncher extends Weapon {
-	
+
 	// icon for weapon
 	Image icon;
-	
+
 	public MineLauncher(int x, int y, int rx, int ry, Ship c){
 		super(x,y,rx,ry,c);
-		
+
 		// natural abilities
 		addNaturalAbility(new RecoilAbility(this));
 		//addNaturalAbility(new SpreadAbility(4,this));
-		
+
 		icon = Util.instance.loadImage("assets/mines_icon.gif");
 	}
 	public Image getIcon(){
 		return icon;
 	}
 	public int getDefaultDamage(){
-		return 5;	
+		return 5;
 	}
 	public void draw(Graphics g){
 	}
 	public void fireProjectile(double heading){
-		new BasicMine(x, y, heading, getDamage(), this );	
+		new Mine(x, y, heading, getDamage(), this );
 	}
 	public int getDefaultCooldown(){
-		return 300;	
+		return 300;
 	}
 }
